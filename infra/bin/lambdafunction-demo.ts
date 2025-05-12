f380579588f34c23f186decce51eef18273a065d
Emily <emily.zy.lin@gmail.com>
2025-05-11 22:50:48 -0500

    create a S3 bucket

--- infra/bin/lambdafunction-demo.ts
+++ infra/bin/lambdafunction-demo.ts
@@ -2,10 +2,14 @@
 import * as cdk from "aws-cdk-lib";
 import { LambdafunctionDemoStack } from "../lib/LambdafunctionDemoStack";
 import { LambdaApiGatewayStack } from "../lib/LambdaApiGatewayStack";
+import { S3Stack } from "../lib/S3Stack";
 
 const app = new cdk.App();
+// launch a lambda function which can be visited by URL
 // new LambdafunctionDemoStack(app, "InfraStack", {
-
 // });
 
-new LambdaApiGatewayStack(app, "InfraStack", {}); //app 是一个 cdk.App 实例，它代表的是 整个 CDK 应用程序
+// launch a lambda function with API gateway
+// new LambdaApiGatewayStack(app, "InfraStack", {}); //app 是一个 cdk.App 实例，它代表的是 整个 CDK 应用程序
+
+new S3Stack(app, "Bankingstatusapp", {});
---
+++ infra/lib/S3Stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+
+export class S3Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    //s3 bucket
+    const balancestatuss3 = new s3.Bucket(this, "s3bucketlogicalid", {
+      bucketName: "balancestatus-demo", //保持小写或者中划线
+    });
+  }
+}
