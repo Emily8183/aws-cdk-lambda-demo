1357ea8ffb797190f8e74227b9071db6d174e671
Emily <emily.zy.lin@gmail.com>
2025-05-12 11:48:24 -0500

    add lambda handler to manage bankingstatus s3 bucket

--- infra/lib/S3Stack.ts
+++ infra/lib/S3Stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class S3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -23,6 +24,18 @@ export class S3Stack extends cdk.Stack {
     //add Managed Policy (Lambda 拥有了对所有 S3 bucket 的完整访问权限）
     iambalancestatusrole.addManagedPolicy(
       iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess")
+    );
+
+    //lambda function
+    const bankingLambdafunction = new lambda.Function(
+      this,
+      "lambdas3logcialid",
+      {
+        handler: "lambda_s3handler.lambda_handler", //must be a string
+        runtime: lambda.Runtime.PYTHON_3_9,
+        code: lambda.Code.fromAsset("../services/"), //从本地路径中 打包 Lambda 函数的代码，并上传到 AWS
+        role: iambalancestatusrole,
+      }
     );
   }
 }
