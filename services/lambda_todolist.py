5a9da82fc9fa7b8ca50a58d3eab39da7692af743
Emily <emily.zy.lin@gmail.com>
2025-05-16 22:56:08 -0500

    create /post request in lambda, pass the test

--- services/lambda_todolist.py
+++ services/lambda_todolist.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import time  # 用来生成Task_id，模拟时间戳
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table("TodosTable") #直接引用
@@ -21,10 +22,8 @@ def lambda_handler(event, context):
     # }
 
     method = event["httpMethod"]
-    # path_params = event.get("pathParameters") or {} # “or {}” 容错
-    # todo_id = path_params.get("id")
-    # body = json.loads(event.get("body", "{}"))
 
+    # GET: passed test in postman
     if method == 'GET':
         try:
             response = table.scan() # 对整个TodoTable做一次全表扫描（Scan），返回所有的 todo 项目
@@ -36,4 +35,34 @@ def lambda_handler(event, context):
             }
         except Exception as e:
             print("Error scanning table:", str(e))
+
+    if method == 'POST':
+        try:
+            #先从http接收到的json字符串，通过json.loads转成python dict
+            body = json.loads(event.get('body', '{}')) 
+
+            todoitem = {
+                "task_id": int(time.time()), #以后应该创建成string, 用UUID
+                "task": body.get("task", ""),
+                "status": body.get("status", False)
+            }
+
+            table.put_item(Item=todoitem)
+
+            return {
+                "statusCode": 201,
+                "headers": { "Content-Type": "application/json" },
+                "body": json.dumps(todoitem)
+            }
+
+        except Exception as e:
+            return {
+                "statusCode": 500,
+                "body": json.dumps({ "error": str(e) })
+            }
+
+    return {
+        "statusCode": 400,
+        "body": "Unsupported method"
+    }
     
